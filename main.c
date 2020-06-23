aee3bdf2dab73469ec73ae9572e14420314c09d0
yuma <yuuma1403@gmail.com>
2020-06-23 11:28:21 +0900

    update

--- main.c
+++ main.c
@@ -17,29 +17,44 @@ void aroundBombCount(char **board, int size);
 
 void displayBoard(char **board, int size);
 
-void play(char **backBoard ,char **showBoard,int size,int bomb);
+int play(char **backBoard, char **showBoard, int size, int bomb);
 
-int checkInput(int x,int y, int mode,int size);
+int checkInput(int x, int y, int mode, int size);
 
-void openSquare(char **backBoard,char **showBoard,int x,int y);
+void openSquare(char **backBoard, char **showBoard, int x, int y);
 
-void checkSquare(char **showBoard,int x,int y);
+void checkSquare(char **showBoard, int x, int y);
+
+int duplicatedInput(char **backboard, char **showBoard, int x, int y);
+
+void displayResult(int isCompleted, int gameTime);
 
 int main() {
-    srand((unsigned int) time(NULL));
     char **backBoard;
     char **showBoard;
     int size;
     int bomb;
+    int isCompleted;
+    int gameTime;
+    time_t start,end;
+
+    srand((unsigned int) time(NULL));
+
     printf("ボードサイズ > ");
     scanf("%d", &size);
     printf("Mの数 > ");
     scanf("%d", &bomb);
+
     backBoard = initBoard(size);
     showBoard = initBoard(size);
-
     createBoard(backBoard, size, bomb);
-    play(backBoard,showBoard,size,bomb);
+
+    start = time(NULL);
+    isCompleted = play(backBoard, showBoard, size, bomb);
+    end = time(NULL);
+    gameTime = (int)(end-start);
+
+    displayResult(isCompleted,gameTime);
 
 
     return 0;
@@ -105,56 +120,90 @@ void aroundBombCount(char **board, int size) {
 }
 
 
-void play(char **backBoard ,char **showBoard,int size,int bomb){
-    int x,y,mode;
+int play(char **backBoard, char **showBoard, int size, int bomb) {
+    int x, y, mode;
     int openCount = 0;
     int isCompleted = FALSE;
     displayBoard(showBoard, size);
     while (!isCompleted) {
-        printf("x y mode");
-        scanf("%d %d %d", &x,&y,&mode);
-        if(checkInput(x,y,mode,size) != TRUE){
-            printf("不正な値です．もう一度入力してください．");
+        printf("x y mode > ");
+        scanf("%d %d %d", &x, &y, &mode);
+        if (checkInput(x, y, mode, size)) {
+            printf("予期しない値です．x座標 y座標 モード(0:オープン，1:マーク)の順にもう一度入力してください．\n");
             continue;
         }
 
+        if (duplicatedInput(backBoard, showBoard, x, y)) {
+            printf("すでにそのマスはオープンしています．\n");
+            continue;
+        }
 
-        if (mode == 0){
-            openSquare(backBoard,showBoard,x,y);
+        if (mode == 0) {
+            openSquare(backBoard, showBoard, x, y);
+            if (showBoard[y][x] == 'M') {
+                displayBoard(showBoard, size);
+                break;
+            }
             openCount++;
         } else {
-            checkSquare(showBoard,x,y);
+            checkSquare(showBoard, x, y);
         }
 
-        if (openCount == size*2 - bomb){
-            int isCompleted = TRUE;
+        if (openCount == size * 2 - bomb) {
+            isCompleted = TRUE;
         }
-
-        displayBoard(showBoard,size);
+        displayBoard(showBoard, size);
     }
+
+    return isCompleted;
 }
 
 void displayBoard(char **board, int size) {
+    printf("\n  ");
     for (int i = 0; i < size; ++i) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", i);
         for (int j = 0; j < size; ++j) {
-            printf("%c", board[i][j]);
+            printf("%c ", board[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-int checkInput(int x,int y, int mode,int size){
-    if ((0 <= x && x < size) && (0 <= y && y < size) && (mode == 0 || mode == 1)){
+int checkInput(int x, int y, int mode, int size) {
+    if ((0 <= x && x < size) && (0 <= y && y < size) && (mode == 0 || mode == 1)) {
+        return FALSE;
+    } else {
+        return TRUE;
+    }
+}
+
+void openSquare(char **backBoard, char **showBoard, int x, int y) {
+    showBoard[y][x] = backBoard[y][x];
+}
+
+void checkSquare(char **showBoard, int x, int y) {
+    showBoard[y][x] = '?';
+}
+
+int duplicatedInput(char **backboard, char **showBoard, int x, int y) {
+    if (backboard[y][x] == showBoard[y][x]) {
         return TRUE;
     } else {
         return FALSE;
     }
 }
 
-void openSquare(char **backBoard,char **showBoard,int x,int y){
-    showBoard[y][x] = backBoard[y][x];
-}
-
-void checkSquare(char **showBoard,int x,int y){
-    showBoard[y][x] = '?';
+void displayResult(int isCompleted,int gameTime){
+    if (isCompleted){
+        printf("ゲームクリア！\n");
+    } else {
+        printf("ゲームオーバー！\n");
+    }
+    printf("記録:%d秒",gameTime);
 }
